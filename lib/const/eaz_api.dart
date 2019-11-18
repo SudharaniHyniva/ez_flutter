07d9b261a515c74d32ec657edd1b70ae9ef13e23
RaviThejaP <ravitheja@hyniva.com>
2019-11-18 17:53:26 +0530

    api file

---
+++ lib/const/eaz_api.dart
@@ -0,0 +1,10 @@
+
+   const String send_SMS = "/api/sendSMS/submit";
+   const String change_password = "/api/changePassword";
+   const String forgot_password = "/api/verifyForgotPasswordOTP";
+   const String send_OTP = "/api/forgotPasswordSendOTP";
+   const String change_password1 = "/api/changePassword";
+   const String login = "/api/authenticate";
+   const String classAssignment = "/api/class/assignment/";
+   const String applyLeaves = "/api/leaves/submit";
+   const String deleteLeaves = "/api/leaveDelete/";
