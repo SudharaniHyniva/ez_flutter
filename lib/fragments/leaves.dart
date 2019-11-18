d61cd0d9030ba31c9e74e69a34e1bf08b9a4dbec
RaviThejaP <ravitheja@hyniva.com>
2019-11-18 17:51:28 +0530

    created view leave page

--- lib/fragments/leaves.dart
+++ lib/fragments/leaves.dart
@@ -1,84 +1,70 @@
-import "package:flutter/material.dart";
-import 'package:login/const/color_const.dart';
-import 'package:login/const/page_name_const.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:login/fragments/StaffLeaves.dart';
+import 'package:login/fragments/studentLeaves.dart';
+
+import 'MyLeaves.dart';
 
 class Leaves extends StatefulWidget {
   @override
-  _Leaves createState() => _Leaves();
+  _TabsDemoState createState() => _TabsDemoState();
 }
-class _Leaves extends State<Leaves>
-    with SingleTickerProviderStateMixin {
-  List<Widget> _tabTwoParameters() => [
-        Tab(
-          text: "My Leaves",
-        ),
-        Tab(
-          text: "Student Leaves",
-        ),
-        Tab(
-          text: "Staff Leaves",
-        ),
-      ];
 
-  TabBar _tabBarLabel() => TabBar(
-        tabs: _tabTwoParameters(),
-        labelColor: RED,
-        labelPadding: EdgeInsets.symmetric(vertical: 10),
-        labelStyle: TextStyle(fontSize: 20),
-        unselectedLabelColor: BLUE_LIGHT,
-        unselectedLabelStyle: TextStyle(fontSize: 14),
-        onTap: (index) {
-          var content = "";
-          switch (index) {
-            case 0:
-              content = "Student Attendance";
-              break;
-            case 1:
-              content = "Staff Attendance";
-              break;
-            case 2:
-              content = "Staff Attendance";
-              break;
-            default:
-              content = "Other";
-              break;
-          }
-          print("You are clicking the $content");
-        },
-      );
+class _TabsDemoState extends State<Leaves>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this, initialIndex: 0);
+    _tabController.addListener(_handleTabIndex);
+  }
+
+  @override
+  void dispose() {
+    _tabController.removeListener(_handleTabIndex);
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void _handleTabIndex() {
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(PageName.LEAVES),
-      ),
-      body: DefaultTabController(
-        length: 3,
-        child: Column(
-          children: <Widget>[
-            Container(
-              constraints: BoxConstraints.expand(height: 60),
-              child: _tabBarLabel(),
-            ),
-            Expanded(
-              child: Container(
-                child: TabBarView(children: [
-                  Container(
-                    child: Text("Currently there are no leave request for you"),
-                  ),
-                  Container(
-                    child: Text("Currently there are no staff leave request"),
-                  ),
-                  Container(
-                    child: Text("Currently there are no student leave request"),
-                  ),
-                ]),
+    return SafeArea(
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Leaves'),
+
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: [
+              Tab(
+                text: "My Leaves",
               ),
-            )
+              Tab(
+                text: "Student Leaves",
+              ),
+              Tab(
+                text: "Staff Leaves",
+              ),
+            ],
+          ),
+        ), //   floatingActionButton: _buildFloatingActionButton(context),
+        body: TabBarView(
+          children: <Widget>[
+            new MyLeaves(),
+            new ViewStudentLeaves(),
+            new ViewStaffLeaves(),
           ],
+          controller: _tabController,
         ),
+        //floatingActionButton: _bottomButtons(),
       ),
     );
   }
