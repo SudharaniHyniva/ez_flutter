944e7187c5ae8b5285277c13abb7fc8bc23d1f32
RaviThejaP <ravitheja@hyniva.com>
2019-11-23 20:32:49 +0530

    changed the dropdown option

--- lib/fragments/MyLeaves.dart
+++ lib/fragments/MyLeaves.dart
@@ -15,10 +15,8 @@ class MyLeaves extends StatefulWidget {
   @override
   createState() => _ViewEvents();
 }
-
 class _ViewEvents extends State<MyLeaves> {
   String _status = 'no-action';
-
   final formKey = GlobalKey<FormState>();
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -33,7 +31,6 @@ class _ViewEvents extends State<MyLeaves> {
   }
 
   //Getting the List of tasks
-  //final String uri = 'https://eazyschool.in//api/leaves/319398/A';
   Map<String, String> headers = {
     HttpHeaders.authorizationHeader: "f2e25125db9926be9731678f5c5f05e4804a85d8",
     HttpHeaders.acceptHeader: "application/json",
@@ -42,7 +39,8 @@ class _ViewEvents extends State<MyLeaves> {
   Future<List<LeavesList>> _fetchUsers() async {
     SharedPreferences _accountId = await SharedPreferences.getInstance();
     var id = _accountId.getString("saved_accountId") ?? "";
-    var response = await http.get(apiURL+"/api/leaves/"+id+"/A", headers: headers);
+    var response =
+        await http.get(apiURL + "/api/leaves/" + id + "/A", headers: headers);
     if (response.statusCode == 200) {
       final items =
           json.decode(response.body)['leavesList'].cast<Map<String, dynamic>>();
@@ -87,21 +85,22 @@ class _ViewEvents extends State<MyLeaves> {
                 shrinkWrap: true,
                 children: snapshot.data
                     .map((user) => ListTile(
-                          leading: Icon(
+                          /*leading:  Icon(
                             Icons.report,
                             size: 50,
                             color: Colors.blue,
-                          ),
-                          title: Text(user.description,
+                          ),*/
+                          title: Text(
+                              user.description +
+                                  "\n" +"Days: "+
+                                  user.leavesCount.toString(),
                               style: new TextStyle(
                                 fontSize: 25.0,
                                 color: Colors.black,
                               )),
                           subtitle: Text(
-                            "From:- " +
-                                user.startDate.substring(0, 10) +
-                                "\n" +
-                                "To:-     " +
+                            user.startDate.substring(0, 10) +
+                                " To " +
                                 user.endDate.substring(0, 10) +
                                 "\n",
                             style: new TextStyle(
@@ -122,60 +121,15 @@ class _ViewEvents extends State<MyLeaves> {
                                   color: Colors.red,
                                   iconSize: 30.0,
                                   onPressed: () {
-                                    {
-                                      int _leaveId = user.id;
-                                      final form = formKey.currentState;
-                                      if (form.validate()) {
-                                        form.save();
-                                        final snackbar = SnackBar(
-                                          duration: Duration(seconds: 30),
-                                          content: Row(
-                                            children: <Widget>[
-                                              NativeLoadingIndicator(),
-                                              Text("  Logging In...")
-                                            ],
-                                          ),
-                                        );
-                                        _scaffoldKey.currentState
-                                            .showSnackBar(snackbar);
-
-                                        setState(
-                                            () => this._status = 'loading');
-
-                                        _auth
-                                            .deleteLeave(
-                                          leaveId: _leaveId.toString().trim(),
-                                        )
-                                            .then((result) {
-                                          try {
-                                            if (result) {
-                                              Navigator.of(context)
-                                                  .pushReplacementNamed(
-                                                      '/home');
-                                              Scaffold.of(context).showSnackBar(
-                                                new SnackBar(
-                                                  content: new Text(
-                                                      'You have deleted leave successfully!'),
-                                                ),
-                                              );
-                                            } else {
-                                              setState(() =>
-                                                  this._status = 'rejected');
-                                              showAlertPopup(
-                                                  context,
-                                                  'Unable to delete leave',
-                                                  _auth.errorMessage);
-                                            }
-
-                                            _scaffoldKey.currentState
-                                                .hideCurrentSnackBar();
-                                          } catch (e) {
-                                            print('Error with URL: $e');
-                                          }
-                                        });
-                                      }
-                                    }
-                                  }),
+                                    int _leaveId = user.id;
+                                    SharedPreferences.getInstance()
+                                        .then((prefs) {
+                                      prefs.setString("delete_leave_id",
+                                          _leaveId.toString());
+                                    });
+                                    showAlertDialog(context);
+                                  }
+                                  ),
                             ],
                           ),
                         ))
@@ -184,5 +138,73 @@ class _ViewEvents extends State<MyLeaves> {
             },
           ),
         ));
+  }
+
+  showAlertDialog(BuildContext context) {
+    final _auth = ScopedModel.of<AuthModel>(context, rebuildOnChange: true);
+    // set up the buttons
+    /*Widget cancelButton = FlatButton(
+      child: Text("Cancel"),
+      onPressed: () {},
+    );*/
+    Widget continueButton = FlatButton(
+      child: Text(
+        "Delete",
+        style: new TextStyle(color: Colors.red),
+      ),
+      onPressed: () {
+        {
+          final form = formKey.currentState;
+          if (form.validate()) {
+            form.save();
+            final snackbar = SnackBar(
+              duration: Duration(seconds: 30),
+              content: Row(
+                children: <Widget>[
+                  NativeLoadingIndicator(),
+                  Text("Deleting...")
+                ],
+              ),
+            );
+            _scaffoldKey.currentState.showSnackBar(snackbar);
+            setState(() => this._status = 'loading');
+            _auth
+                .deleteLeave(
+                    //  leaveId: _leaveId.toString().trim(),
+                    )
+                .then((result) {
+              try {
+                if (result) {
+                  Navigator.of(context).pushReplacementNamed('/home');
+                } else {
+                  setState(() => this._status = 'rejected');
+                  showAlertPopup(
+                      context, 'Unable to delete leave', _auth.errorMessage);
+                }
+                _scaffoldKey.currentState.hideCurrentSnackBar();
+              } catch (e) {
+                print('Error with URL: $e');
+              }
+            });
+          }
+        }
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("Confirm.."),
+      content: Text("Would you like delete selected leave??"),
+      actions: [
+        //cancelButton,
+        continueButton,
+      ],
+    );
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
   }
 }
